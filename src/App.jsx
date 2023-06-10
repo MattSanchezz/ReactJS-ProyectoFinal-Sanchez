c2eca13027811683fc39451dd132dcb8453e2616
MattSanchezz <mati.sanchez.2001@hotmail.com>
2023-06-10 00:31:53 -0300

    Finalizando el checkout de la pagina, conexion con base de datos

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailCont
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CarritoProvider } from './context/CarritoContext';
 import Cart from './components/Cart/Cart';
+import Checkout from './components/Checkout/Checkout';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
             <Route path='/categoria/:idCategoria' element={<ItemListContainer />} />
             <Route path='/item/:idItem' element={<ItemDetailContainer />} />
             <Route path='/cart' element = {<Cart/>} />
+            <Route path='/checkout' element = {<Checkout/>} />
             <Route path='*' element={<h2>Sitio en Construccion</h2>} />
           </Routes>
         </CarritoProvider>
